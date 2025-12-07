4683da1431807924f3a0d157bc9e4fdf8b6a1d3e
Mashopy <eliasgheeraert@gmail.com>
2025-12-07 02:44:30 +0100

    aidl: audio: Drop sounddose as a optional interface
    No MediaTek device seems to implement this interface.

--- aidl/audio/service.cpp
+++ aidl/audio/service.cpp
@@ -130,10 +130,6 @@ int main(int /* argc */, char* /* argv */ []) {
             "android.hardware.bluetooth.audio-impl",
             "createIBluetoothAudioProviderFactory",
         },
-        {
-            "android.hardware.audio.sounddose-vendor-impl",
-            "createISoundDoseFactory",
-        },
     };
     // clang-format on
 
